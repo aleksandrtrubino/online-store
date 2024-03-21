1b987f1941a66ad6bb07448660bd58bb3cf5b5be
Karavaev2024 <dima_karavaev04@mail.ru>
2024-03-21 21:31:13 +0300

    Add pickup_points and purchase_status tables and PickupPoint and PurchaseStatus class

---
+++ src/main/java/ru/vistar/PurchaseStatus.java
@@ -0,0 +1,39 @@
+package ru.vistar;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "purchase_statuses")
+public class PurchaseStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "purchase_status_seq")
+    @Column(name = "purchase_status_id")
+    private Long id;
+
+    @Column(name = "purchase_status_name")
+    private String name;
+
+    PurchaseStatus(){}
+
+    public PurchaseStatus(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
